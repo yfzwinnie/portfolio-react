2c4d6276a49a95b9f04da98a3e110966b7755aa8
Winnie Zhang <yfz.winnie@gmail.com>
2020-07-08 11:12:01 -0400

    add skills component

---
+++ src/components/Skills.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Skills = () => {
+  return (
+    <section id="skills">
+      <h1>Skills</h1>
+      <div className="content">
+        <div className="skill">Front-End</div>
+        <div className="skill">Back End</div>
+        <div className="skill">Design</div>
+        <div className="skill">Version Control</div>
+      </div>
+    </section>
+  );
+};
+
+export default Skills;
